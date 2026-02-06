0607bf2e156860cb42057178ee8061d497712d31
saentiagu <santii8cho@gmail.com>
2026-02-05 23:23:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,29 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Estado simple en memoria
+const salas = {};
+
 app.get("/", (req, res) => {
   res.send("Servidor WereSan activo 🐺🔥");
+});
+
+// Crear sala
+app.get("/crear-sala", (req, res) => {
+  const codigo = Math.random()
+    .toString(36)
+    .substring(2, 7)
+    .toUpperCase();
+
+  salas[codigo] = {
+    jugadores: [],
+    creada: Date.now()
+  };
+
+  res.json({
+    ok: true,
+    sala: codigo
+  });
 });
 
 app.listen(PORT, () => {
